123a08cb71b54c8ddd8ff2554b21413423f88145
ShifaNasarV <114069135+ShifaNasarV@users.noreply.github.com>
2023-11-27 16:28:59 +0530

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -29,7 +29,7 @@ def tumor_detection(img, model):
     return "Tumor Detected" if res else "No Tumor"
 
 # Streamlit App
-st.title("Deep Prediction Hub")
+st.title("Deep-Learning-Predictions")
 
 # Choose between tasks
 task = st.radio("Select Task", ("Sentiment Classification", "Tumor Detection"))
